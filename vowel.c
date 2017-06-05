374a2568aae0aeeb603c89894e6cedea4e603f7f
Pavithra-31 <paviyugesh@gmail.com>
2017-06-05 15:39:33 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -6,11 +6,11 @@ printf("enter the variable");
 scanf("%c",&ch);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
-printf(" \n is vowel",ch);
+printf(" \n is vowel");
 }
 else
 {
-printf("\n is constant",ch);
+printf("\n is constant");
 }
 return 0;
 }
