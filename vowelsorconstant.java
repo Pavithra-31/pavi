24a754b972a22bee066d6d03081f69919396dad7
Pavithra-31 <paviyugesh@gmail.com>
2017-06-14 09:38:35 +0530

    Create vowelsorconstant.java

---
+++ vowelsorconstant.java
@@ -0,0 +1,37 @@
+public class vowels{
+public static void main(String []args){
+char vowel='a';
+switch(vowel){
+case 'a':
+System.out.println("the character is vowel");
+break;
+case 'e':
+System.out.println("the character is vowel");
+break;
+case 'i':
+System.out.println("the character is vowel");
+break;
+case 'o':
+Sytem.out.println("the character is vowel");
+break;
+case 'u':
+System.out.println("the character is vowel");
+break;
+case 'A':
+System.out.println("the characte is vowel");
+break;
+case 'E':
+System.out.println("the character is vowel");
+break;
+case 'I':
+System.out.println("the character is vowel");
+break;
+case 'O':
+System.out.println("the character is vowel");
+break;
+case 'U':
+System.out.println("the character is vowel");
+break;
+default :
+System.out.println("the character is constant");
+
