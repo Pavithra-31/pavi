41428a3ee05888e8859780b0aab3a001bf11171f
Pavithra-31 <paviyugesh@gmail.com>
2017-06-14 09:54:02 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,9 @@
+public class alphabet{
+public static void main(String []args){
+char ch='e';
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+System.out.println("it is alphabet");
+}
+else{
+System.out.println("it is not alphabet");
+}
