91111515eccf6cadd088cf5167a1f2361384453f
Pavithra-31 <paviyugesh@gmail.com>
2017-06-14 17:34:06 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,42 @@
+package programs;
+
+public class vowels {
+	public static void main(String []args){
+		char vowel='g';
+		switch(vowel){
+		case 'a':
+		System.out.println("the character is vowel");
+		break;
+		case 'e':
+		System.out.println("the character is vowel");
+		break;
+		case 'i':
+		System.out.println("the character is vowel");
+		break;
+		case 'o':
+		System.out.println("the character is vowel");
+		break;
+		case 'u':
+		System.out.println("the character is vowel");
+		break;
+		case 'A':
+		System.out.println("the characte is vowel");
+		break;
+		case 'E':
+		System.out.println("the character is vowel");
+		break;
+		case 'I':
+		System.out.println("the character is vowel");
+		break;
+		case 'O':
+		System.out.println("the character is vowel");
+		break;
+		case 'U':
+		System.out.println("the character is vowel");
+		break;
+		default:
+			System.out.println("the character is constant");
+
+}
+	}
+}
