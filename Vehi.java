8ba9c530cfad631fe3872374109a013467f8a440
Pavithra-31 <paviyugesh@gmail.com>
2017-06-30 15:16:44 +0530

    Add files via upload

---
+++ Vehi.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Vehi {
+	Vehi(){
+		System.out.println("Bike is created");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vehi v=new Vehi();
+	}
+
+}
+
