170079ef7e5e03cc7f11b527f07415617d2d8db4
Pavithra-31 <paviyugesh@gmail.com>
2017-07-14 09:36:24 +0530

    Add files via upload

---
+++ convertstring.java
@@ -0,0 +1,17 @@
+package program;
+import java.util.Scanner;
+
+public class convertstring {
+
+	public static void main(String[] args) {
+		String str;
+		Scanner input=new Scanner(System.in);
+		str=input.nextLine();
+		int number=Integer.parseInt(str);
+		System.out.println(number);
+		input.close();
+		
+
+	}
+
+}
