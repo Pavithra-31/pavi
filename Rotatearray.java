792ad2f825221ef907156bad46ed6044280b11c6
Pavithra-31 <paviyugesh@gmail.com>
2017-08-05 16:07:45 +0530

    Add files via upload

---
+++ Rotatearray.java
@@ -0,0 +1,18 @@
+package program;
+import java.util.Scanner;
+
+public class Rotatearray {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner input=new Scanner(System.in);
+		n=input.nextInt();
+		int[]arr=new int[n];
+		for(int i=0;i<arr.length;i++){
+			arr[i]=input.nextInt();
+		
+		
+	
+	}
+
+}
